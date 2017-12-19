66ea3ebb3f92a8a9bea4c25a9b946f897dde3ab8
mreyers <matthewreyers3333@gmail.com>
2017-12-19 09:33:51 -0800

    Basic filtering of excess scraped data

--- Combine.R
+++ Combine.R
@@ -7,3 +7,10 @@ library(tidyverse)
 
 Combine <- read.csv("CombineDataComp.csv", strip.white = TRUE, as.is = TRUE, header = TRUE)
 head(Combine)
+
+# Fix the read problem with year by grabbing substring from yearresults
+names(Combine)[1:4] <- c("start.url", "yearResults", "yearResultshref", "Year")
+
+CombineMetricYears <- str_detect(Combine$yearResults, "[0-9]{4} NFL Combine Results")
+Combine[CombineMetricYears, "Year"] <-  substr(Combine[CombineMetricYears, "yearResults"], 1, 4)
+Combine <- Combine[Combine$Year != "null",]
