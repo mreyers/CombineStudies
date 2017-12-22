8d220af6b3949dc2cd9064a9303ff6eec280b0bd
mreyers <matthewreyers3333@gmail.com>
2017-12-21 16:57:06 -0800

    A bit more cleaning and plotting start

--- Combine.R
+++ Combine.R
@@ -4,13 +4,24 @@
 
 # Libraries
 library(tidyverse)
+library(stringr)
 
 Combine <- read.csv("CombineDataComp.csv", strip.white = TRUE, as.is = TRUE, header = TRUE)
 head(Combine)
 
 # Fix the read problem with year by grabbing substring from yearresults
 names(Combine)[1:4] <- c("start.url", "yearResults", "yearResultshref", "Year")
-
 CombineMetricYears <- str_detect(Combine$yearResults, "[0-9]{4} NFL Combine Results")
 Combine[CombineMetricYears, "Year"] <-  substr(Combine[CombineMetricYears, "yearResults"], 1, 4)
-Combine <- Combine[Combine$Year != "null",]
+
+# Remove the entries from the wrong data files and select relevant columns
+Combine <- Combine[Combine$Year != "null",] %>% select(-start.url, -yearResults, -yearResultshref)
+
+# Rename the activity columns to easier names
+names(Combine)[c(5, 6, 10, 11)] <- c("Height", "Weight", "VertLeap", "BroadJump")
+
+# NFL is American so standard units are inches, pounds, and yards
+
+# Some introductory plots to get an idea of what each stat means
+playerSize <- ggplot(data = Combine, aes(x = Year, y = Height))+
+              geom_boxplot()
